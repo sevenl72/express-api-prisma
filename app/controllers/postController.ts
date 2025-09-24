e830fd03158a765a91e447b8243634da58e55528
NelFeast <starsbots6@gmail.com>
2025-09-24 11:20:41 +0700

    Add Post controller and update Prisma schema

---
+++ app/controllers/postController.ts
@@ -0,0 +1,19 @@
+export const createPost = async (req: Request, res: Response) => {
+    // Implementation for creating a post
+}
+
+export const getPosts = async (req: Request, res: Response) => {
+    // Implementation for retrieving posts
+}
+
+export const getPostById = async (req: Request, res: Response) => {
+    // Implementation for retrieving a single post by ID
+}
+
+export const updatePost = async (req: Request, res: Response) => {
+    // Implementation for updating a post
+}
+
+export const deletePost = async (req: Request, res: Response) => {
+    // Implementation for deleting a post
+}
